a1c8d6d4f18462f47f3f31f797dee675660361d7
CABNlT <jkistler28@windhamsd.org>
2025-06-05 09:08:18 -0400

    Update install_homebrew.sh

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -3,3 +3,4 @@ git clone https://github.com/Homebrew/brew homebrew
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
+git clone https://github.com/CABNlT/Brew-install.git
