d8d35e38d5ac8542e026f2a6d88db6ba0b054af3
CABNlT <jkistler28@windhamsd.org>
2025-06-05 09:46:03 -0400

    Update install_homebrew.sh

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -5,6 +5,6 @@ eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 rm ~/.zshrc
-git clone https://github.com/CABNlT/Brew-install.git
 mv ~/Brew-install/.zshrc ~
 brew install --cask --appdir ~/Desktop steam --no-quarantine
+rm ~/Brew-install
