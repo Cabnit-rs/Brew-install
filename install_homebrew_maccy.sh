639ea4dc86677b5b4d8d8cde759258e92e76bd9c
CABNlT <jkistler28@windhamsd.org>
2025-06-06 08:50:46 -0400

    Update install_homebrew_maccy.sh

--- install_homebrew_maccy.sh
+++ install_homebrew_maccy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 git clone https://github.com/Homebrew/brew homebrew
-eval "$(homebrew/bin/brew shellenv)"
+eval "$(~/homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 rm ~/.zshrc
