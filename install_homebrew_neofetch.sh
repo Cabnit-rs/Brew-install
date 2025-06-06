061778351379319230b75c470376585810aa1a51
CABNlT <jkistler28@windhamsd.org>
2025-06-06 07:50:41 -0400

    Create install_homebrew_neofetch.sh

---
+++ install_homebrew_neofetch.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+git clone https://github.com/Homebrew/brew homebrew
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+rm ~/.zshrc
+mv ~/Brew-install/.zshrc ~
