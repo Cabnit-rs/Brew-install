1149c6b918cdd4a5088cce101226558a02cf6a06
CABNlT <jkistler28@windhamsd.org>
2025-06-06 08:46:20 -0400

    Update install_homebrew_maccy.sh

--- install_homebrew_maccy.sh
+++ install_homebrew_maccy.sh
@@ -4,5 +4,7 @@ eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 rm ~/.zshrc
+git clone https://github.com/CABNlT/Brew-install.git
 mv ~/Brew-install/.zshrc ~
+rm ~/Brew-install/
 brew install --cask --appdir ~/Desktop maccy --no-quarantine
