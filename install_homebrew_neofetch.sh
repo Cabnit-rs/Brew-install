769363c85cae8e991440eefd077f99bac2834ef6
CABNlT <jkistler28@windhamsd.org>
2025-06-06 08:57:08 -0400

    Update install_homebrew_neofetch.sh

--- install_homebrew_neofetch.sh
+++ install_homebrew_neofetch.sh
@@ -3,9 +3,9 @@ git clone https://github.com/Homebrew/brew homebrew
 eval "$(~/homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
-rm ~/.zshrc
+rm -rf ~/.zshrc
 git clone https://github.com/CABNlT/Brew-install.git
 mv ~/Brew-install/.zshrc ~
-rm ~/Brew-install/
+rm -rf ~/Brew-install/
 brew install neofetch
 eval "$(~/homebrew/bin/brew shellenv)"
